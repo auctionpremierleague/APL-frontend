f1ebac516305957815e6fb0156cf002a6a37f38f
AS <arunsalgia@gmail.com>
2020-08-12 23:51:01 +0530

    Test

--- backend/routes/playerstat.js
+++ backend/routes/playerstat.js
@@ -7,7 +7,7 @@ const { enabled } = require('debug');
 router.use('/', function(req, res, next) {
 res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.send("Player Statistics controller under development");
+  res.send("Player Statistics controller under development.   ");
   next();
 });
 
