28cad68158512e43abc94dc7eb9aa3562b75c178
akshama <akshamasalgia@gmail.com>
2020-09-01 20:56:06 +0530

    test

---
+++ client/build/precache-manifest.0a66e1acb48dc31b04cb4c57e803a02c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c57e12cf5c2880c3de20c47458d70c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c037f04ad8a81297d0a0",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "045b85f8c958cc8c28f8",
+    "url": "/static/js/2.a8e1c814.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.a8e1c814.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c037f04ad8a81297d0a0",
+    "url": "/static/js/main.6935537c.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
