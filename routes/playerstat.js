afebe643a2b39d7dd74465148849e786a8c730df
Akshama Salgia <akshama.salgia@oracle.com>
2020-09-18 00:21:59 +0530

    1 minute

--- routes/playerstat.js
+++ routes/playerstat.js
@@ -1192,9 +1192,9 @@ cron.schedule('*/2 * * * *', () => {
 });
 
 
-cron.schedule('*/2 * * * * ', () => {
+cron.schedule('*/1 * * * * ', () => {
   // schedule to continuous update to client
-  console.log('==========running every 2 minute. Sending socket');
+  console.log('==========running every 1 minute. Sending socket');
     _group = 1;
     _tournament = "IPL2020"
     statMax(0, doMaxRun, SENDSOCKET);
